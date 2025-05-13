5b2f86ddb8b772bc519481f95f7fd5f6ed67777d
VishmithaNadeeshan <vishmithanadeeshan20041111@gmail.com>
2025-05-13 17:31:21 +0530

    first commit

---
+++ src/app/pages/get-all-employees/get-all-employees.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all-employees',
+  imports: [],
+  templateUrl: './get-all-employees.component.html',
+  styleUrl: './get-all-employees.component.css'
+})
+export class GetAllEmployeesComponent {
+
+}
